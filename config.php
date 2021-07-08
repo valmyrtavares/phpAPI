ed2d38cb2cef51318b9ad56946b773949963a3a2
Valmyr Tavares <valmyr.lima@millennium.com.br>
2021-07-08 18:32:07 -0300

    BUG 9/7

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ $db_name = "devnotes";
 $db_user = "root";
 $db_pass = "Amanda@230406";
 
-$pdo = new PDO("mysql:db_name=$db_name;host=$db_host",$db_user, $db_pass);
+$pdo = new PDO("mysql:name=$db_name;host=$db_host",$db_user, $db_pass);
 
 $array = [];
